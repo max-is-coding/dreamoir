7c7d5dfb076d54ef9f665081c25924b3e54549d8
Max (Amber) Hilton <107532278+max-is-coding@users.noreply.github.com>
2022-09-04 13:51:01 +1200

    wefhjnowejiowerf

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,8 @@
-const config = require('./knexfile').development
-// eslint-disable-next-line no-unused-vars
+// const config = require('./knexfile').development
+// // eslint-disable-next-line no-unused-vars
+// const connection = require('knex')(config)
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 function getCards(db = connection) {
